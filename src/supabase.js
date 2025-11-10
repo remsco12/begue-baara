3a12f68d4f8c344d4b3a949f1f34c5aa1b758e16
remsco12 <remsco12@users.noreply.github.com>
2025-11-10 21:00:47 +0000

    Fix deployment

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'VOTRE_URL_SUPABASE'
-const supabaseAnonKey = 'VOTRE_CLE_ANON_PUBLIC'
+const supabaseUrl = 'https://rrmewqgykmebthmjuuul.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJybWV3cWd5a21lYnRobWp1dXVsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI3OTg4MDksImV4cCI6MjA3ODM3NDgwOX0.9AHiDs3rm2soY84zGT8p_7e0OB2q0k9RZSOlMJcr7hc'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
